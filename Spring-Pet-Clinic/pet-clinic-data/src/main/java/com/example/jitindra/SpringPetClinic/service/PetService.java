3fc9aad342623cda2d1ab183d2d5f6d542f78622
Jitindra Fartiyal <jayfartiyal@hotmail.com>
2018-08-02 18:41:51 +0530

    Refactored Service Interface into Common Base Interface/Jitindra Fartiyal. Closes #11

--- Spring-Pet-Clinic/pet-clinic-data/src/main/java/com/example/jitindra/SpringPetClinic/service/PetService.java
+++ Spring-Pet-Clinic/pet-clinic-data/src/main/java/com/example/jitindra/SpringPetClinic/service/PetService.java
@@ -2,13 +2,6 @@ package com.example.jitindra.SpringPetClinic.service;
 
 import com.example.jitindra.SpringPetClinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet,Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
