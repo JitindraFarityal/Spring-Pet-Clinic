70d961a746643815c2100a222530873a1c5e501c
Jitindra Fartiyal <jayfartiyal@hotmail.com>
2018-08-02 14:05:37 +0530

    Adding Base Entity. Closes #10

--- Spring-Pet-Clinic/pet-clinic-data/src/main/java/com/example/jitindra/SpringPetClinic/model/Person.java
+++ Spring-Pet-Clinic/pet-clinic-data/src/main/java/com/example/jitindra/SpringPetClinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.example.jitindra.SpringPetClinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- Spring-Pet-Clinic/pet-clinic-data/src/main/java/com/example/jitindra/SpringPetClinic/model/PetType.java
+++ Spring-Pet-Clinic/pet-clinic-data/src/main/java/com/example/jitindra/SpringPetClinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.example.jitindra.SpringPetClinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
